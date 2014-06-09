2f776d5cf6e69287336ba4f0bd18785f1f71e9a9
Mohcine Chraibi <m.chraibi@gmail.com>
2014-06-09 11:11:22 +0200

    add some statistics indicating how much bytes we parse and how much bytes are expected to be read. Actually, the parser misses some bytes. Comparison, with the function getpart5data from smokeview/IOpart.c reveals no hint, what is exactly missing..

--- read_ptr5.f90
+++ read_ptr5.f90
@@ -1,3 +1,4 @@
+! TODO: check getpart5data in smokeview/IOpart.c
 
 ! This program reads a prt5-file (FDS Version 5.5) and write the trajectories to an output file in the fllowing format
 ! frame index x y z
@@ -69,9 +70,10 @@ CHARACTER (len=100) :: in_file
 CHARACTER (len=100) :: out_file
 CHARACTER (len=100) :: s_out_file = "tmp.dat"
 CHARACTER  :: dummy
-INTEGER ONE_INTEGER, VERSION_NUMBER, N_EVAC, N_PART, ZERO_INTEGER, EVAC_N_QUANTITIES
-INTEGER ios, I, NN, N, NPLIM, IZERO
+INTEGER ONE_INTEGER, VERSION_NUMBER, N_EVAC, N_PART, ZERO_INTEGER, EVAC_N_QUANTITIES, file_size, read_size
+INTEGER ios, I, NN, N, NPLIM, IZERO, EVAC
 INTEGER :: frame, status, is_error
+INTEGER indianswitch ! I don't know what to do with this. Got it from smokeview/IOpart.c
 real ::   counter
 
 REAL(FB) :: T
@@ -97,9 +99,8 @@ IF ( LEN_TRIM(out_file) == 0) THEN
    WRITE (0,*) "USAGE: ./parser <inputfile> <outputfile>"
    STOP
 ENDIF
-
-PRINT *, "INFO: inputfile = <", TRIM(in_file), ">"
-
+INQUIRE(FILE=in_file, SIZE=file_size)
+PRINT *, "INFO: inputfile = <", TRIM(in_file), ">", "  |  size = ", file_size, " bytes"
 
 OPEN(unit = 9, file = in_file, form = "unformatted", status = "old",   iostat = ios)
 IF (ios .NE. 0) THEN
@@ -116,18 +117,30 @@ ENDIF
 !================== sort file with respect to <id> and than to <frame> (linux-commands) ================== 
 sort_command = 'sort -k1,1 -k2,2 -n ' // TRIM(out_file)  //' > ' // TRIM(s_out_file)
 mv_command = 'mv ' // TRIM(s_out_file) // ' ' // TRIM(out_file)
+read_size = 0
+
 !================== read header ================== 
 READ(9) ONE_INTEGER      ! Integer 1 to check Endian-ness
 READ(9) VERSION_NUMBER   ! FDS version number
 READ(9) N_EVAC           ! Number of PARTicle classes
+EVAC = 1                 ! TODO: When is EVAC == 0? 
+read_size = read_size + sizeof(ONE_INTEGER) + sizeof(VERSION_NUMBER) + sizeof(N_EVAC)
 !================== read header ================== 
 WRITE(6,'(A, I2)') " #  ONE_INTEGER = ", ONE_INTEGER
 WRITE(6,'(A, F4.2)') " #  VERSION_NUMBER = ", VERSION_NUMBER/100.
 WRITE(6,'(A, I2)') " #  N_EVAC = ", N_EVAC
 
+IF (ONE_INTEGER /= 1) then 
+   indianswitch = 1
+ELSE
+   indianswitch = 0 
+ENDIF
+
 DO N=1,N_EVAC
    READ(9) EVAC_N_QUANTITIES, ZERO_INTEGER  ! ZERO_INTEGER is a place holder
+   read_size = read_size + sizeof(EVAC_N_QUANTITIES) + sizeof(ZERO_INTEGER)
    WRITE(6,'(A, I2)') " #  EVAC_N_QUANTITIES = ", EVAC_N_QUANTITIES
+   WRITE(6,'(A, I2)') " #  ZERO_INTEGER = ", ZERO_INTEGER
    ALLOCATE(NAME(EVAC_N_QUANTITIES))
    ALLOCATE(UNITS(EVAC_N_QUANTITIES))
 
@@ -135,10 +148,15 @@ DO N=1,N_EVAC
    DO NN = 1, EVAC_N_QUANTITIES
       READ(9) NAME(NN)   !OUTPUT_QUANTITY(EVAC_QUANTITIES_INDEX(NN))%NAME
       READ(9) UNITS(NN)  !OUTPUT_QUANTITY(EVAC_QUANTITIES_INDEX(NN))%UNITS
+      read_size = read_size + sizeof( NAME(NN) ) + sizeof( UNITS(NN) )
+      WRITE(6,*) "#  NAME  =  ", NAME(NN)
+      WRITE(6,*) "#  UNITS = ", UNITS(NN)
+!      skip_local = 2*(numtypes_temp[0]+numtypes_temp[1])*(8 + 30);
+!      returncode=FSEEK(PART5FILE,skip_local,SEEK_CUR);
+!      ignore NAME and UNITS (30 each) and for each entity fortran writes 4 before and 4 after (8). 
    ENDDO
 ENDDO ! N_EVAC
-WRITE(6,*) "#  NAME  =  ", NAME
-WRITE(6,*) "#  UNITS = ", UNITS
+
 frame = 0 
 counter = 1
 is_error = 0 ! 1 if something went wrong after allucating the arrays
@@ -146,11 +164,13 @@ is_error = 0 ! 1 if something went wrong after allucating the arrays
 DOFILE: DO
    frame = frame + 1
    READ(9), T           ! the time T as 4 byte real
+   read_size = read_size + sizeof(T)
    !WRITE(6, *) "#  TIME = ", T
    ! print * , "------- frame = ", frame
    DO N = 1, N_EVAC
       ! print * , "======="
       READ(9), NPLIM    ! Number of particles in the PART class
+      read_size = read_size + sizeof(NPLIM)
       IF (frame .eq. 1) then 
          counter = NPLIM
          WRITE(6,*) "#  MAX PEDESTRIANS = ", NPLIM
@@ -178,15 +198,27 @@ DOFILE: DO
          CALL ChkMemErr('DUMP','QP',IZERO)
       ENDIF
 !===================== READ Trajectories ======================
-      READ(9, iostat = ios), (XP(I), I=1, NPLIM), (YP(I), I=1, NPLIM), (ZP(I), I=1, NPLIM), &
-           (AP(I,1),I=1,NPLIM),(AP(I,2),I=1,NPLIM),(AP(I,3),I=1,NPLIM),(AP(I,4),I=1,NPLIM)
+      !if(parti->evac==1) read 7 * NPLIM
+      ! else read 3 * NPLIM 
+      ! TODO: How to know the value parti-evac?
+      IF(EVAC == 1) THEN 
+         READ(9, iostat = ios), (XP(I), I=1, NPLIM), (YP(I), I=1, NPLIM), (ZP(I), I=1, NPLIM), &
+              (AP(I,1),I=1,NPLIM),(AP(I,2),I=1,NPLIM),(AP(I,3),I=1,NPLIM),(AP(I,4),I=1,NPLIM)
+         read_size = read_size + 7*sizeof(XP)
+      ELSE
+         READ(9, iostat = ios), (XP(I), I=1, NPLIM), (YP(I), I=1, NPLIM), (ZP(I), I=1, NPLIM)
+         read_size = read_size + 3*sizeof(XP)
+      ENDIF
+      
 !==============================================================
       IF (ios .NE. 0) THEN
          write (0,*) " ERROR: Could not read trajectories "
          is_error = 1
          EXIT DOFILE !STOP
       ENDIF
-      READ(9, iostat = ios), (TA(I), I=1, NPLIM)  
+      READ(9, iostat = ios), (TA(I), I=1, NPLIM)
+
+      read_size = read_size + sizeof(TA)  
       IF (ios .NE. 0) THEN
          write (0,*) " ERROR: Could not read TA "
          is_error = 1
@@ -205,11 +237,13 @@ DOFILE: DO
       ! QP color information for smokeview
       IF (EVAC_N_QUANTITIES > 0 ) THEN
          READ(9), ((QP(I,NN), I=1, NPLIM), NN=1,  EVAC_N_QUANTITIES)
+         read_size = read_size + sizeof(QP)
       END IF
 
       IF (NPLIM == 1)THEN
          print *, " "
-         print *, "INFO: Close files"
+         print *, "INFO: Close files" 
+         print *, "INFO: Bytes parsed ", read_size, " /", file_size, "(===> missed: ", file_size-read_size,"Bytes)"
          CLOSE(unit = 9)  !input file
          CLOSE(unit = 15) !output file
          PRINT *, sort_command
