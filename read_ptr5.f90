f87d6a7840c6b81c481c06cbcd01328f14128542
Mohcine Chraibi <m.chraibi@gmail.com>
2015-05-11 15:33:21 +0200

    diff size of read file

--- read_ptr5.f90
+++ read_ptr5.f90
@@ -70,7 +70,7 @@ CHARACTER (len=100) :: in_file
 CHARACTER (len=100) :: out_file
 CHARACTER (len=100) :: s_out_file = "tmp.dat"
 CHARACTER  :: dummy
-INTEGER ONE_INTEGER, VERSION_NUMBER, N_EVAC, N_PART, ZERO_INTEGER, EVAC_N_QUANTITIES, file_size, read_size
+INTEGER ONE_INTEGER, VERSION_NUMBER, N_EVAC, N_PART, ZERO_INTEGER, EVAC_N_QUANTITIES, file_size, read_size, INT_SIZE
 INTEGER ios, I, NN, N, NPLIM, IZERO, EVAC
 INTEGER :: frame, status, is_error
 INTEGER indianswitch ! I don't know what to do with this. Got it from smokeview/IOpart.c
@@ -118,13 +118,14 @@ ENDIF
 sort_command = 'sort -k1,1 -k2,2 -n ' // TRIM(out_file)  //' > ' // TRIM(s_out_file)
 mv_command = 'mv ' // TRIM(s_out_file) // ' ' // TRIM(out_file)
 read_size = 0
+INT_SIZE = sizeof(ONE_INTEGER)
 
 !================== read header ================== 
 READ(9) ONE_INTEGER      ! Integer 1 to check Endian-ness
 READ(9) VERSION_NUMBER   ! FDS version number
 READ(9) N_EVAC           ! Number of PARTicle classes
 EVAC = 1                 ! TODO: When is EVAC == 0? 
-read_size = read_size + sizeof(ONE_INTEGER) + sizeof(VERSION_NUMBER) + sizeof(N_EVAC)
+read_size = read_size + sizeof(ONE_INTEGER) + sizeof(VERSION_NUMBER) + sizeof(N_EVAC) + 6*INT_SIZE
 !================== read header ================== 
 WRITE(6,'(A, I2)') " #  ONE_INTEGER = ", ONE_INTEGER
 WRITE(6,'(A, F4.2)') " #  VERSION_NUMBER = ", VERSION_NUMBER/100.
@@ -138,7 +139,7 @@ ENDIF
 
 DO N=1,N_EVAC
    READ(9) EVAC_N_QUANTITIES, ZERO_INTEGER  ! ZERO_INTEGER is a place holder
-   read_size = read_size + sizeof(EVAC_N_QUANTITIES) + sizeof(ZERO_INTEGER)
+   read_size = read_size + sizeof(EVAC_N_QUANTITIES) + sizeof(ZERO_INTEGER) + 2*INT_SIZE
    WRITE(6,'(A, I2)') " #  EVAC_N_QUANTITIES = ", EVAC_N_QUANTITIES
    WRITE(6,'(A, I2)') " #  ZERO_INTEGER = ", ZERO_INTEGER
    ALLOCATE(NAME(EVAC_N_QUANTITIES))
@@ -148,12 +149,9 @@ DO N=1,N_EVAC
    DO NN = 1, EVAC_N_QUANTITIES
       READ(9) NAME(NN)   !OUTPUT_QUANTITY(EVAC_QUANTITIES_INDEX(NN))%NAME
       READ(9) UNITS(NN)  !OUTPUT_QUANTITY(EVAC_QUANTITIES_INDEX(NN))%UNITS
-      read_size = read_size + sizeof( NAME(NN) ) + sizeof( UNITS(NN) )
+      read_size = read_size + sizeof( NAME(NN) ) + sizeof( UNITS(NN) ) + 4*INT_SIZE
       WRITE(6,*) "#  NAME  =  ", NAME(NN)
       WRITE(6,*) "#  UNITS = ", UNITS(NN)
-!      skip_local = 2*(numtypes_temp[0]+numtypes_temp[1])*(8 + 30);
-!      returncode=FSEEK(PART5FILE,skip_local,SEEK_CUR);
-!      ignore NAME and UNITS (30 each) and for each entity fortran writes 4 before and 4 after (8). 
    ENDDO
 ENDDO ! N_EVAC
 
@@ -164,13 +162,13 @@ is_error = 0 ! 1 if something went wrong after allucating the arrays
 DOFILE: DO
    frame = frame + 1
    READ(9), T           ! the time T as 4 byte real
-   read_size = read_size + sizeof(T)
+   read_size = read_size + sizeof(T) + 2*INT_SIZE
    !WRITE(6, *) "#  TIME = ", T
    ! print * , "------- frame = ", frame
    DO N = 1, N_EVAC
       ! print * , "======="
       READ(9), NPLIM    ! Number of particles in the PART class
-      read_size = read_size + sizeof(NPLIM)
+      read_size = read_size + sizeof(NPLIM) + 2*INT_SIZE
       IF (frame .eq. 1) then 
          counter = NPLIM
          WRITE(6,*) "#  MAX PEDESTRIANS = ", NPLIM
@@ -204,10 +202,10 @@ DOFILE: DO
       IF(EVAC == 1) THEN 
          READ(9, iostat = ios), (XP(I), I=1, NPLIM), (YP(I), I=1, NPLIM), (ZP(I), I=1, NPLIM), &
               (AP(I,1),I=1,NPLIM),(AP(I,2),I=1,NPLIM),(AP(I,3),I=1,NPLIM),(AP(I,4),I=1,NPLIM)
-         read_size = read_size + 7*sizeof(XP)
+         read_size = read_size + 7*sizeof(XP) + 2*INT_SIZE
       ELSE
          READ(9, iostat = ios), (XP(I), I=1, NPLIM), (YP(I), I=1, NPLIM), (ZP(I), I=1, NPLIM)
-         read_size = read_size + 3*sizeof(XP)
+         read_size = read_size + 3*sizeof(XP) + 2*INT_SIZE
       ENDIF
       
 !==============================================================
@@ -217,14 +215,13 @@ DOFILE: DO
          EXIT DOFILE !STOP
       ENDIF
       READ(9, iostat = ios), (TA(I), I=1, NPLIM)
+      read_size = read_size + sizeof(TA) + 2*INT_SIZE
 
-      read_size = read_size + sizeof(TA)  
       IF (ios .NE. 0) THEN
          write (0,*) " ERROR: Could not read TA "
          is_error = 1
          EXIT DOFILE !STOP
       ENDIF
-      ! print *, "TA=", (TA(I), I=1, NPLIM)
     
 !================ WRITE Trajectories ======================
       DO I=1,NPLIM
@@ -237,13 +234,13 @@ DOFILE: DO
       ! QP color information for smokeview
       IF (EVAC_N_QUANTITIES > 0 ) THEN
          READ(9), ((QP(I,NN), I=1, NPLIM), NN=1,  EVAC_N_QUANTITIES)
-         read_size = read_size + sizeof(QP)
+         read_size = read_size + sizeof(QP) + 2*INT_SIZE
       END IF
 
       IF (NPLIM == 1)THEN
          print *, " "
          print *, "INFO: Close files" 
-         print *, "INFO: Bytes parsed ", read_size, " /", file_size, "(===> missed: ", file_size-read_size,"Bytes)"
+         print *, "INFO: Bytes parsed ", read_size, " /", file_size, "(===> difference ", file_size-read_size,"Bytes)"
          CLOSE(unit = 9)  !input file
          CLOSE(unit = 15) !output file
          PRINT *, sort_command
