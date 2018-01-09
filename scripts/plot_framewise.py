18daf21f0acc04c4908ce5e9961961f065bd5065
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-01-09 12:55:22 +0100

    python 3

--- scripts/plot_framewise.py
+++ scripts/plot_framewise.py
@@ -1,26 +1,49 @@
+# plots the trajectories framewise and produce png files
+
+from sys import argv
+import os
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
-from sys import argv
 
 
-
-if len(argv)<=1:
-    print ("usage: %s  <filename>"%argv[0])
+if len(argv) <= 1:
+    print("usage: %s  <filename>"%argv[0])
     exit(">>>>exit<<<<")
 filename = argv[1]
 
-print ("load file %s..."%filename) 
+print("load file %s..."%filename)
 D = np.loadtxt(filename)
 
 frames = np.unique(D[:, 0])
+xmin = np.min(D[:, 2])
+xmax = np.max(D[:, 2])
+ymin = np.min(D[:, 3])
+ymax = np.max(D[:, 3])
+
 for frame in frames:
-    d = D[D[:,0] == frame]
-    x = d[:,2]
-    y = d[:,3]
-    plt.plot(x, y, "o", color='b',ms=10)
-    plt.xlim([-1100,1200])
-    plt.ylim([-200,700])
-    
-    plt.savefig("%.3d.png"%frame )
-    print ("%.3d.png"%frame)
+    d = D[D[:, 0] == frame]
+    x = d[:, 2]
+    y = d[:, 3]
+    plt.plot(x, y, "o", color='b', ms=10)
+    plt.xlim([xmin-0.5, xmax+0.5])
+    plt.ylim([ymin-0.5, ymax+0.5])
+
+    plt.savefig("%.3d.png"%frame)
+    print("%.3d.png"%frame)
     plt.clf()
+
+make_movie = r'ffmpeg -y -framerate 16 -pattern_type  glob -i "*.png" -r 25 -f mpeg -vcodec mpeg1video -ar 48000 -b:v 5000k -b:a 128k -acodec mp2 -ar 44100 -ac 1  demo.mpg'
+
+try:
+    err = os.system(make_movie)
+except err:
+    print("command does not exist. err = %d"%err)
+
+png_files = glob.glob("*.png")
+
+if png_files:
+    print("remove png files")
+    
+for f in png_files:
+    os.remove(f)
--- scripts/plot_traj.py
+++ scripts/plot_traj.py
@@ -5,11 +5,12 @@ from sys import argv
 
 
 if len(argv)<=1:
-	print "usage: %s  <filename>"%argv[0]
+	print("usage: %s  <filename>"%argv[0])
 	exit(">>>>exit<<<<")
+
 filename = argv[1]
 
-print 'load file ...', filename 
+print('load file ...', filename)
 D = loadtxt(filename)
 
 peds = unique(D[:,1])
@@ -18,7 +19,7 @@ for ped in peds:
 	if ped not in range(10):
 		continue
 	if ped%10 == 0:
-		print "ped = %d"%ped
+		print("ped = %d"%ped)
 	d = D[D[:,1] == ped]
 	x = d[:,2]
 	y = d[:,3]
@@ -30,6 +31,7 @@ ylabel("y [m]")
 name = filename.split(".")[0]
 title(name)
 for end in ['png', 'pdf', 'eps']:
-	print "save figure as <%s.%s>"%(name,end)
-	savefig("%s.%s"%(name, end) )
+    print("save figure as <%s.%s>"%(name,end))
+    savefig("%s.%s"%(name, end))
+
 show()
