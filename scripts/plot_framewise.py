cf5a388343706cb4e85d587a8d442d2b11d515e2
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-01-15 12:42:59 +0100

    Update plot scripts for 3D ploting

--- scripts/plot_framewise.py
+++ scripts/plot_framewise.py
@@ -5,7 +5,7 @@ import os
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
-
+from mpl_toolkits.mplot3d import Axes3D
 
 if len(argv) <= 1:
     print("usage: %s  <filename>"%argv[0])
@@ -15,22 +15,35 @@ filename = argv[1]
 print("load file %s..."%filename)
 D = np.loadtxt(filename)
 
-frames = np.unique(D[:, 0])
+frames = np.unique(D[:, 1])
 xmin = np.min(D[:, 2])
 xmax = np.max(D[:, 2])
 ymin = np.min(D[:, 3])
 ymax = np.max(D[:, 3])
+fig = plt.figure()
+
+z = np.sort(D[:, 4])
+_3D = 0
+if z[0] != z[-1]:
+    ax = fig.add_subplot(111, projection='3d')
+    print("3D")
+    _3D = 1
+
 
 for frame in frames:
-    d = D[D[:, 0] == frame]
+    d = D[D[:, 1] == frame]
     x = d[:, 2]
     y = d[:, 3]
-    plt.plot(x, y, "o", color='b', ms=10)
+    if _3D:
+        z = d[:, 4]
+        plt.plot(x, y, z, "o", color='b', ms=10)
+    else:
+        plt.plot(x, y, "o", color='b', ms=10)
+
     plt.xlim([xmin-0.5, xmax+0.5])
     plt.ylim([ymin-0.5, ymax+0.5])
-
-    plt.savefig("%.3d.png"%frame)
-    print("%.3d.png"%frame)
+    plt.savefig("%.5d.png"%frame)
+    print("%.5d.png"%frame)
     plt.clf()
 
 make_movie = r'ffmpeg -y -framerate 16 -pattern_type  glob -i "*.png" -r 25 -f mpeg -vcodec mpeg1video -ar 48000 -b:v 5000k -b:a 128k -acodec mp2 -ar 44100 -ac 1  demo.mpg'
--- scripts/plot_traj.py
+++ scripts/plot_traj.py
@@ -1,31 +1,44 @@
 from matplotlib.pyplot import *
 from numpy import *
 from sys import argv
+from mpl_toolkits.mplot3d import Axes3D
+
 
 
 
 if len(argv)<=1:
-	print("usage: %s  <filename>"%argv[0])
-	exit(">>>>exit<<<<")
+        print("usage: %s  <filename>"%argv[0])
+        exit(">>>>exit<<<<")
 
 filename = argv[1]
 
 print('load file ...', filename)
 D = loadtxt(filename)
 
-peds = unique(D[:,1])
+peds = unique(D[:,0])
+fig = figure()
 
-for ped in peds:
-	if ped not in range(10):
-		continue
-	if ped%10 == 0:
-		print("ped = %d"%ped)
-	d = D[D[:,1] == ped]
-	x = d[:,2]
-	y = d[:,3]
-	plot(x, y, "-", color='r',lw=2)
+z = np.sort(D[:, 4])
 
+_3D = 0
+if z[0] != z[-1]:
+    ax = fig.add_subplot(111, projection='3d')
+    print("3D")
+    _3D = 1
     
+for ped in peds:
+    if ped%10 == 0:
+        print("ped = %.3d"%ped)
+    d = D[D[:,0] == ped]
+    x = d[:,2]
+    y = d[:,3]
+
+    if _3D:
+        z = d[:, 4]
+        plot(x, y, z, "-", color='r',lw=0.2)
+    else:
+        plot(x, y, "-", color='r',lw=0.2)
+        
 xlabel("x [m]")
 ylabel("y [m]")
 name = filename.split(".")[0]
